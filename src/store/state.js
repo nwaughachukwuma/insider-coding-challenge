45ce63f7b8f039b06b0c8458864203639a5b93e5
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2020-01-17 16:54:44 +0100

    define constants

---
+++ src/store/state.js
@@ -0,0 +1,30 @@
+// Key codes
+export const ENTER       = 13;
+export const LEFT_ARROW  = 37;
+export const RIGHT_ARROW = 39;
+export const SPACE       = 32;
+
+
+// object shapes
+export const SQUARE   = 1;
+export const TRIANGLE = 2;
+export const CIRCLE   = 3;
+
+// weights
+export const MIN_WEIGHT = 1;
+export const MAX_WEIGHT = 10;
+
+// scale
+export const SCALE_STEP = 0.08;
+
+// falling intervals
+export const MAX_FALLING_INTERVAL = 10;
+export const MIN_FALLING_INTERVAL = 5;
+
+// bending angles
+export const MAX_BENDING_ANGLE = 30;
+export const MIN_BENDING_ANGLE = -30;
+
+// teeter-totter board
+export const BOARD_HEIGHT = 6;
+export const BOARD_WIDTH  = 100;
